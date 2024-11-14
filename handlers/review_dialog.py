8e06632ad7f0d2704c9270720461958745ef1d50
Erzhan-ish <isenbekoverzan1@gmail.com>
2024-11-14 14:52:42 +0600

    hw_4

--- handlers/review_dialog.py
+++ handlers/review_dialog.py
@@ -21,43 +21,65 @@ async def start_review(callback_query: types.CallbackQuery, state: FSMContext):
 
 @review_router.message(RestourantReview.name)
 async def process_name(message: types.Message, state: FSMContext):
-    await state.set_state(RestourantReview.phone_number)
+    name = message.text
+    if not name.isalpha():
+        await message.answer("Вводите только буквами!")
+        return
+
+    if len(name) > 15:
+        await message.answer("Вводите не больше 15 символов!")
+        return
+
     await state.update_data(name=message.text)
+    await state.set_state(RestourantReview.phone_number)
     await message.answer("Ваш номер телефона")
 
 
 @review_router.message(RestourantReview.phone_number)
 async def process_phone_number(message: types.Message, state: FSMContext):
+    number = message.text
+    if not number.isdigit():
+        await message.answer("Вводите только номер(цифрами)!")
+        return
+    await state.update_data(phone_number=message.text)
     await state.set_state(RestourantReview.visit_date)
     await message.answer("Дата вашего визита")
-    await state.update_data(phone_number=message.text)
-
 
 @review_router.message(RestourantReview.visit_date)
 async def process_food_rating(message: types.Message, state: FSMContext):
-    await state.set_state(RestourantReview.food_rating)
-    await message.answer("Как оцениваете качество еды\n"
-                         "1-4 - плохо\n"
-                         "5-7 - хорошо\n"
-                         "7-10 - отлично")
     await state.update_data(visit_date=message.text)
+    await state.set_state(RestourantReview.food_rating)
+    kb = types.ReplyKeyboardMarkup(
+        keyboard=[
+            [
+                types.KeyboardButton(text="Плохо"),
+                types.KeyboardButton(text="Хорошо"),
+                types.KeyboardButton(text="Отлично")
+            ]
+        ]
+    )
+    await message.answer("Как оцениваете качество еды", reply_markup=kb)
 
 
-@review_router.message(RestourantReview.food_rating)
+
+@review_router.message(RestourantReview.food_rating, F.text.in_(["Плохо","Хорошо","Отлично"]))
 async def process_cleanliness_rating(message: types.Message, state: FSMContext):
-    await state.set_state(RestourantReview.cleanliness_rating)
-    await message.answer("Как оцениваете чистоту заведенияе\n"
-                         "1-4 - плохо\n"
-                         "5-7 - хорошо\n"
-                         "7-10 - отлично")
+    kb = types.ReplyKeyboardRemove()
     await state.update_data(food_rating=message.text)
+    await state.set_state(RestourantReview.cleanliness_rating)
+    await message.answer("Как оцениваете чистоту заведения\n"
+                         "от 1 до 10", reply_markup=kb)
 
 
 @review_router.message(RestourantReview.cleanliness_rating)
 async def process_extra_comments(message: types.Message, state: FSMContext):
+    ball = message.text
+    if not ball.isdigit() or int(ball) < 1 or int(ball) > 10:
+        await message.answer("Вводите только цифры от 1 до 10!")
+        return
+    await state.update_data(cleanliness_rating=message.text)
     await state.set_state(RestourantReview.extra_comments)
     await message.answer("Дополнительные коментарии/Жалобы")
-    await state.update_data(cleanliness_rating=message.text)
 
 
 @review_router.message(RestourantReview.extra_comments)
