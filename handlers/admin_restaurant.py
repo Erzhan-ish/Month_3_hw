7fe7dd2b9b39c590a2da3dcee25edf09cefb9ce7
Erzhan-ish <isenbekoverzan1@gmail.com>
2024-11-25 21:44:43 +0600

    hw_7

--- handlers/admin_restaurant.py
+++ handlers/admin_restaurant.py
@@ -1,4 +1,4 @@
-from aiogram import Router, F, types
+from aiogram import Router, F, types, Dispatcher
 from aiogram.fsm.state import State, StatesGroup, default_state
 from aiogram.fsm.context import FSMContext
 from aiogram.filters import Command
@@ -15,6 +15,28 @@ class Dishes(StatesGroup):
     name = State()
     price = State()
     cooking_time = State()
+    categories = State()
+
+class Dishes_cat(StatesGroup):
+    name = State()
+
+@admin_rest_router.message(Command("newcategories"))
+async def new_categories(message: types.Message, state: FSMContext):
+    await state.set_state(Dishes_cat.name)
+    await message.answer("Задайте новую категорию:")
+
+@admin_rest_router.message(Dishes_cat.name)
+async def process_cat(message: types.Message, state: FSMContext):
+    category = message.text
+    database.execute(
+        query="""
+            INSERT INTO dish_categories (name)
+            VALUES (?)
+            """,
+        params=(category, )
+                     )
+    await message.answer("Категория добавлена в БД")
+    await state.clear()
 
 # @admin_rest_router.message(F.text.in_(["стоп", "stop"]))
 # async def stop(message: types.Message, state: FSMContext):
@@ -41,15 +63,39 @@ async def process_price(message: types.Message, state: FSMContext):
 @admin_rest_router.message(Dishes.cooking_time)
 async def process_cooking_time(message: types.Message, state: FSMContext):
     await state.update_data(cooking_time=message.text)
-    await message.answer("Блюдо добавлено")
+    all_category = database.fetch("SELECT * FROM dish_categories")
+    if not all_category:
+        await message.answer("Нет ни одной категории")
+        await state.clear()
+        return
+    kb = types.ReplyKeyboardMarkup(
+        keyboard=[
+            [types.KeyboardButton(text=dishes["name"]) for dishes  in all_category]
+        ]
+    )
+    await state.set_state(Dishes.categories)
+    await message.answer("Задайте категорие блюда: ", reply_markup=kb)
+
+@admin_rest_router.message(Dishes.categories)
+async def process_categories(message: types.Message, state: FSMContext):
+    kb = types.ReplyKeyboardRemove()
+    print(message.text)
+    category_name = database.fetch(
+        query="SELECT name FROM dish_categories WHERE name = ?",
+        params=(message.text,)
+    )
+    if not category_name:
+        await message.answer("Вы напечатали несуществующию категорию в БД")
+        return
+    await state.update_data(categories=category_name[0]["name"])
     data = await state.get_data()
     print(data)
     await state.clear()
-
+    await message.answer("Блюдо добавлено", reply_markup=kb)
     database.execute(
         query="""
-        INSERT INTO dishes (name, price, cooking_time)
-        VALUES(?, ?, ?)
+        INSERT INTO dishes (name, price, cooking_time, categories)
+        VALUES(?, ?, ?, ?)
         """,
-        params=(data["name"], data["price"], data["cooking_time"]),
+        params=(data["name"], data["price"], data["cooking_time"], data["categories"]),
     )
